f22071846a439a76f792a0197b47e63e2323ddaf
Xiaogang HE <hexg@princeton.edu>
2015-01-01 15:42:24 -0500

    Add the docstring inside the function

--- PyScripts/process_CDE.py
+++ PyScripts/process_CDE.py
@@ -3,9 +3,15 @@
 import	numpy			as np
 import	re
 
-# baseDir		= '/Users/hexg/Dropbox/Study/Princeton_2014-2015_Fall/APC524/APC_Project_HEXG/Data'
-
 def	getVarDes(baseDir,CDEFileName):
+	"""
+	Get variable's label and description from the .CDE file
+
+	Returns
+	------
+	Variables in DSSAT summary output: Dictionary
+	"""
+	
 	sumOutCDE	= file('%s/%s'%(baseDir,CDEFileName)).readlines()
 	sumOutCDEArr	= [map(str,re.split(r'\t+',sumOutCDE[i].strip())) for i in range(5,73)]
 	sumOutCDEDic	= {sumOutCDEArr[i][0]:(sumOutCDEArr[i][1],sumOutCDEArr[i][2]) for i in range(np.shape(sumOutCDEArr)[0])}
