e6381c6867c8a0415bed304a9950a952431aa0d0
Xiaogang HE <hexg@princeton.edu>
2015-01-05 21:35:12 -0500

    Creat a new folder

---
+++ Tools/CSM_precompile.sh
@@ -0,0 +1,9 @@
+gfortran -w -ffixed-form -fd-lines-as-comments -ffixed-line-length-none -c ModuleDefs.f90
+gfortran -w -ffixed-form -fd-lines-as-comments -ffixed-line-length-none -c OPHEAD.f90
+gfortran -w -ffixed-form -fd-lines-as-comments -ffixed-line-length-none -c SoilMixing.f90
+gfortran -w -ffixed-form -fd-lines-as-comments -ffixed-line-length-none -c SLigCeres.f90
+gfortran -w -ffixed-form -fd-lines-as-comments -ffixed-line-length-none -c OPSUM.f90
+gfortran -w -ffixed-form -fd-lines-as-comments -ffixed-line-length-none -c SC_CNG_mods.f90
+
+
+
